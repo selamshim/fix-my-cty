2f6f6c9940d00cba5c70be68099b34a8e8eb78fa
selamshimelis <shimeselam@gmail.com>
2020-11-02 16:14:49 +0300

    signupdone

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router  } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from "../../shared/services/auth.service"; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ){ }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if(this.authService.isLoggedIn !== true) {
+      this.router.navigate(['sign-in'])
+    }
     return true;
   }
-  
+
 }
